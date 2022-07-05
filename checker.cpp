5df474be1dde51e036ee413f52e23b21b16b85d6
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-07-05 21:45:33 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -5,49 +5,37 @@
 #define TEMPERATURE_UPPER_BOUNDARY  (45)
 #define SOC_LOWER_BOUNDARY          (20)
 #define SOC_UPPER_BOUNDARY          (80)
-#define CHARGE_RATE_BOUNDARY        (0.8)
+const float CHARGE_RATE_BOUNDARY = 0.8;
 
-using namespace std;
+using namespace     std;
 
 bool isBatteryTemperatureOk(float temperature);
 bool isBatteryStateOfChargeOk(float soc);
 bool isBatteryChargeRateOk(float chargeRate);
 
 bool isBatteryTemperatureOk(float temperature){
-  bool temperatureReturn = false;
+  bool temperatureReturn = true;
   if((temperature < TEMPERATURE_LOWER_BOUNDARY) || (temperature > TEMPERATURE_UPPER_BOUNDARY)) {
     cout << "Temperature out of range!\n";
     temperatureReturn = false;
-  }
-  else {
-    cout << "Temperature is in range\n";
-    temperatureReturn = true;
   }
   return temperatureReturn;
 }
 
 bool isBatteryStateOfChargeOk(float soc){
-  bool socReturn = false;
+  bool socReturn = true;
   if((soc < SOC_LOWER_BOUNDARY) || (soc > SOC_UPPER_BOUNDARY)) {
     cout << "State of Charge out of range!\n";
     socReturn = false;
-  }
-  else {
-    cout << "State of Charge is in range\n";
-    socReturn = true;
   }
   return socReturn;
 }
 
 bool isBatteryChargeRateOk(float chargeRate){
-  bool chargeRateReturn = false;
+  bool chargeRateReturn = true;
   if(chargeRate > CHARGE_RATE_BOUNDARY) {
     cout << "Charge Rate out of range!\n";
     chargeRateReturn = false;
-  }
-  else {
-    cout << "Charge Rate is in range\n";
-    chargeRateReturn = true;
   }
   return chargeRateReturn;
 }
@@ -68,6 +56,6 @@ int main() {
   assert(isBatteryStateOfChargeOk(SOC_UPPER_BOUNDARY + 1) == false);
 
   assert(isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY - 1) == true);
-  assert(isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY) == false);
+  assert(isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY) == true);
   assert(isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY +1) == false);
 }
