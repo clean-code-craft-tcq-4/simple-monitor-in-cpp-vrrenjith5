1de198be1c5d6a5bbcd98d56a740052218a35819
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-07-13 07:35:08 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -9,22 +9,48 @@ typedef enum Language{
 static int logLanguage = English;
 
 using namespace     std;
-const float TEMPERATURE_LOWER_BOUNDARY = 0;
-const float TEMPERATURE_UPPER_BOUNDARY = 45;
-const float SOC_LOWER_BOUNDARY = 20;
-const float SOC_LOWER_BOUNDARY_WARNING = 24;
-const float SOC_UPPER_BOUNDARY_WARNING = 76;
-const float SOC_UPPER_BOUNDARY = 80;
-const float CHARGE_RATE_BOUNDARY = 0.8;
 
-class ElectricVehicle{
+class Comparison{
   public:
     bool isLessThanOrEqual(float value, float boundary);
     bool isGreaterThanOrEqual(float value, float boundary);
     bool isInRange(float value, float lowerBoundary, float upperBoundary);
 };
 
-class Battery: public ElectricVehicle {
+bool Comparison :: isLessThanOrEqual(float value, float boundary){
+ bool result = ((value <= boundary)?(true):(false));
+ if (!result){
+   cout<<"value is greater than boundary"<<endl;
+ }
+ return result;
+}
+
+bool Comparison :: isGreaterThanOrEqual(float value, float boundary){
+ bool result = ((value >= boundary)?(true):(false));
+ if (!result){
+   cout<<"value is less than boundary"<<endl;
+ }
+ return result;
+}
+
+bool Comparison :: isInRange(float value, float lowerBoundary, float upperBoundary){
+ return (isLessThanOrEqual(value, upperBoundary) && isGreaterThanOrEqual(value, lowerBoundary));
+}
+
+Comparison comparisonObject;
+
+class Battery{
+  public:
+    const float TEMPERATURE_LOWER_BOUNDARY = 0;
+    const float TEMPERATURE_UPPER_BOUNDARY = 45;
+    const float SOC_LOWER_BOUNDARY = 20;
+    const float SOC_LOWER_BOUNDARY_WARNING = 24;
+    const float SOC_UPPER_BOUNDARY_WARNING = 76;
+    const float SOC_UPPER_BOUNDARY = 80;
+    const float CHARGE_RATE_BOUNDARY = 0.8;
+};
+
+class BatteryManagementSystem : public Battery{
   public:
     bool isBatteryTemperatureOk(float temperature);
     bool isBatteryStateOfChargeOk(float soc);
@@ -33,36 +59,16 @@ class Battery: public ElectricVehicle {
     bool isBatteryChargeRateOk(float chargeRate);
 };
 
-bool ElectricVehicle :: isLessThanOrEqual(float value, float boundary){
- bool result = ((value <= boundary)?(true):(false));
- if (!result){
-   cout<<"value is greater than boundary"<<endl;
- }
- return result;
-}
-
-bool ElectricVehicle :: isGreaterThanOrEqual(float value, float boundary){
- bool result = ((value >= boundary)?(true):(false));
- if (!result){
-   cout<<"value is less than boundary"<<endl;
- }
- return result;
-}
-
-bool ElectricVehicle :: isInRange(float value, float lowerBoundary, float upperBoundary){
- return (isLessThanOrEqual(value, upperBoundary) && isGreaterThanOrEqual(value, lowerBoundary));
-}
-
-bool Battery :: isBatteryTemperatureOk(float temperature){
- bool result = isInRange(temperature, TEMPERATURE_LOWER_BOUNDARY, TEMPERATURE_UPPER_BOUNDARY);
+bool BatteryManagementSystem :: isBatteryTemperatureOk(float temperature){
+ bool result = comparisonObject.isInRange(temperature, TEMPERATURE_LOWER_BOUNDARY, TEMPERATURE_UPPER_BOUNDARY);
  if(!result) {
    cout << "Temperature "<<temperature<< " is out of range!"<<endl;
  }
  return result;
 }
 
-void Battery :: batterySocWarningLow(float soc) {
- bool result = isInRange(soc, SOC_LOWER_BOUNDARY, SOC_LOWER_BOUNDARY_WARNING);
+void BatteryManagementSystem :: batterySocWarningLow(float soc) {
+ bool result = comparisonObject.isInRange(soc, SOC_LOWER_BOUNDARY, SOC_LOWER_BOUNDARY_WARNING);
  if(result) {
    switch(logLanguage){
      case English :
@@ -78,15 +84,15 @@ void Battery :: batterySocWarningLow(float soc) {
  }
 }
 
-void Battery :: batterySocWarningHigh(float soc) {
- bool result = isInRange(soc, SOC_UPPER_BOUNDARY_WARNING, SOC_UPPER_BOUNDARY);
+void BatteryManagementSystem :: batterySocWarningHigh(float soc) {
+ bool result = comparisonObject.isInRange(soc, SOC_UPPER_BOUNDARY_WARNING, SOC_UPPER_BOUNDARY);
  if(result) {
    cout << "Warning: Approaching charge-peak!"<<endl;
  }
 }
 
-bool Battery :: isBatteryStateOfChargeOk(float soc){
- bool result = isInRange(soc, SOC_LOWER_BOUNDARY, SOC_UPPER_BOUNDARY);
+bool BatteryManagementSystem :: isBatteryStateOfChargeOk(float soc){
+ bool result = comparisonObject.isInRange(soc, SOC_LOWER_BOUNDARY, SOC_UPPER_BOUNDARY);
  batterySocWarningLow(soc);
  batterySocWarningHigh(soc);
  if(!result) {
@@ -95,8 +101,8 @@ bool Battery :: isBatteryStateOfChargeOk(float soc){
  return result;
 }
 
-bool Battery :: isBatteryChargeRateOk(float chargeRate){
-  bool result = isLessThanOrEqual(chargeRate, CHARGE_RATE_BOUNDARY);
+bool BatteryManagementSystem :: isBatteryChargeRateOk(float chargeRate){
+  bool result = comparisonObject.isLessThanOrEqual(chargeRate, CHARGE_RATE_BOUNDARY);
   if(!result) {
     cout << "Charge Rate "<<chargeRate<< " is out of range!" <<endl;
   }
@@ -104,22 +110,24 @@ bool Battery :: isBatteryChargeRateOk(float chargeRate){
 }
 
 int main() {
-  Battery BatteryObject;
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_LOWER_BOUNDARY - 1) == false);
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_LOWER_BOUNDARY) == true);
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_LOWER_BOUNDARY + 1) == true);
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_UPPER_BOUNDARY - 1) == true);
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_UPPER_BOUNDARY) == true);
-  assert(BatteryObject.isBatteryTemperatureOk(TEMPERATURE_UPPER_BOUNDARY + 1) == false);
+  BatteryManagementSystem bmsObject;
+  Battery batteryObject;
 
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_LOWER_BOUNDARY - 1) == false);
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_LOWER_BOUNDARY) == true);
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_LOWER_BOUNDARY + 1) == true);
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_UPPER_BOUNDARY - 1) == true);
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_UPPER_BOUNDARY) == true);
-  assert(BatteryObject.isBatteryStateOfChargeOk(SOC_UPPER_BOUNDARY + 1) == false);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_LOWER_BOUNDARY - 1) == false);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_LOWER_BOUNDARY) == true);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_LOWER_BOUNDARY + 1) == true);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_UPPER_BOUNDARY - 1) == true);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_UPPER_BOUNDARY) == true);
+  assert(bmsObject.isBatteryTemperatureOk(batteryObject.TEMPERATURE_UPPER_BOUNDARY + 1) == false);
 
-  assert(BatteryObject.isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY - 1) == true);
-  assert(BatteryObject.isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY) == true);
-  assert(BatteryObject.isBatteryChargeRateOk(CHARGE_RATE_BOUNDARY +1) == false);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_LOWER_BOUNDARY - 1) == false);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_LOWER_BOUNDARY) == true);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_LOWER_BOUNDARY + 1) == true);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_UPPER_BOUNDARY - 1) == true);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_UPPER_BOUNDARY) == true);
+  assert(bmsObject.isBatteryStateOfChargeOk(batteryObject.SOC_UPPER_BOUNDARY + 1) == false);
+
+  assert(bmsObject.isBatteryChargeRateOk(batteryObject.CHARGE_RATE_BOUNDARY - 1) == true);
+  assert(bmsObject.isBatteryChargeRateOk(batteryObject.CHARGE_RATE_BOUNDARY) == true);
+  assert(bmsObject.isBatteryChargeRateOk(batteryObject.CHARGE_RATE_BOUNDARY +1) == false);
 }
